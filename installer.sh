4599e0eb283ff27a76b7b59bdb5de8b1d2e9dd67
Fabio Gibson <fabiogibson@hotmail.com>
2017-06-06 19:38:33 -0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -165,7 +165,7 @@ apt_install 				\
 	libssl-dev			\
 	libpq-dev			\
 	libreadline-gplv2-dev           \ 
-	bzip2-devel                     \
+	bzip2                     \
 	tk-dev                          \
 	libgdbm-dev                     \
 	libc6-dev                       \
