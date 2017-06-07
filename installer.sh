43369a6728690ef4858c327d956aefd6e15e9508
Fabio Gibson <fabiogibson@hotmail.com>
2017-06-07 11:59:01 -0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -38,8 +38,7 @@ install_pyenv() {
 }
 	
 install_ohmyzsh() {
-	echo Installing Oh-My-Zsh...
-	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" > /dev/null
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 }
 
 install_powerline_fonts() {
