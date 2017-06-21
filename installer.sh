d62e9a9c41123dd25fbbce0a14ce301f2a0349e3
Fabio Gibson <fabiogibson@hotmail.com>
2017-06-21 18:35:32 -0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,4 +1,22 @@
 #!/bin/sh
+
+##################################################
+# Helpers
+##################################################
+cmd_exists() {
+	command -v "$@" > /dev/null 2>&1
+}
+
+git_clone() {
+	env git clone --depth=1 $1 $2 || {
+  		printf "Error: git clone of $1 failed\n"
+  		exit 1
+  	}
+}
+
+##################################################
+# Package Managers
+##################################################
 pacman_install() {
 	for pack in "$@"; do	
 		printf Installing $pack\n
@@ -19,97 +37,10 @@ yaourt_install() {
 	done
 }
 
-cmd_exists() {
-	command -v "$@" > /dev/null 2>&1
-}
-
-install_pyenv() {
-	export PYENV_VIRTUALENV_DISABLE_PROMPT=1
-	export PYENV_ROOT="${HOME}/.pyenv"
-
-	if [ ! -d "$PYENV_ROOT" ]; then
-		echo Installing PyEnv...
-		export PATH="${PYENV_ROOT}/bin:$PATH"
-		(curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash) > /dev/null
-		(git clone https://github.com/yyuu/pyenv-virtualenvwrapper.git ${PYENV_ROOT}/plugins/pyenv-virtualenvwrapper) > /dev/null
-	else
-		echo Skipping PyEnv
-	fi
-
-	eval "$(pyenv init -)"
-}
-	
-install_ohmyzsh() {
-	ZSH=~/.oh-my-zsh
-	umask g-w,o-w
-
-	env git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git $ZSH || {
-  		printf "Error: git clone of oh-my-zsh repo failed\n"
-  		exit 1
-  	}
-}
-
-install_powerline_fonts() {
-	if [ ! -f "$HOME/.local/share/fonts/Meslo LG L DZ Regular for Powerline.ttf" ]; then
-		echo Installing Powerline fonts...
-		
-		git clone --depth=1 https://github.com/powerline/fonts.git ./fonts/ || {
-			printf "Error: git clone of powerline fonts failed\n"
-			exit 1
-		}
-			
-		./fonts/install.sh "Meslo LG L DZ Regular for Powerline" 2>&1
-		rm -rf ./fonts
-	else
-		echo Skipping Powerline fonts...
-	fi
-}
-
-install_dotfiles() {
-	for file in zshrc zshenv tmux.conf; do
-		echo Creating $HOME/.$file
-		curl -s -o $HOME/.$file https://raw.githubusercontent.com/fabiogibson/dev-machine/master/dotfiles/$file 2>&1	
-	done
-	
-	if command -v xfce4-about  1>/dev/null; then
-		mkdir -p  $HOME/.config/xfce4/terminal
-		curl -s -o $HOME/.config/xfce4/terminal/terminalrc https://raw.githubusercontent.com/fabiogibson/dev-machine/master/dotfiles/terminalrc 2>&1
-	fi
-}
-
-install_ranger() {
-	if ! cmd_exists ranger; then
-		sudo git clone git://git.savannah.nongnu.org/ranger.git /opt/ranger
-		sudo ln -s /opt/ranger/ranger.py /usr/local/bin/ranger
-	fi
-}
-
-
-
-install_wrk() {
-	if ! cmd_exists wrk; then
-		git clone https://github.com/wg/wrk.git > /dev/null
-		cd wrk && make > /dev/null
-		sudo cp wrk /usr/local/bin
-		cd - && rm -rf ./wrk
-	fi
-}
-
 npm_install() {
 	for pack in "$@"; do
 		sudo npm install -g $pack > /dev/null
 	done
-}
-
-create_virtual_env() {
-	pyenv install -s $1
-
-	if [ ! -d "$PYENV_ROOT/versions/$2" ]; then
-		echo Creating virtualenv $2 with Python $1
-		pyenv virtualenv $1 $2
-	else
-		echo Skipping virtualenv $2
-	fi
 }
 
 pip_install() {
@@ -123,10 +54,97 @@ pip_install() {
 	pyenv deactivate
 }
 
+##################################################
+# Installation Scripts
+##################################################
+install_pyenv() {
+	export PYENV_VIRTUALENV_DISABLE_PROMPT=1
+	export PYENV_ROOT="${HOME}/.pyenv"
+
+	if [ ! -d "$PYENV_ROOT" ]; then
+		echo Installing PyEnv...
+		export PATH="${PYENV_ROOT}/bin:$PATH"
+		(curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash) > /dev/null
+		git_clone https://github.com/yyuu/pyenv-virtualenvwrapper.git ${PYENV_ROOT}/plugins/pyenv-virtualenvwrapper
+	else
+		echo Skipping PyEnv
+	fi
+
+	eval "$(pyenv init -)"
+}
+	
+install_ohmyzsh() {
+	umask g-w,o-w
+	git_clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+}
+
+install_dotfiles() {
+	for file in zshrc zshenv tmux.conf; do
+		echo Creating $HOME/.$file
+		curl -s -o $HOME/.$file https://raw.githubusercontent.com/fabiogibson/dev-machine/master/dotfiles/$file 2>&1	
+	done
+	
+	if cmd_exists xfce4-about; then
+		mkdir -p  $HOME/.config/xfce4/terminal
+		curl -s -o $HOME/.config/xfce4/terminal/terminalrc https://raw.githubusercontent.com/fabiogibson/dev-machine/master/dotfiles/terminalrc 2>&1
+	fi
+}
+
+install_wrk() {
+	if ! cmd_exists wrk; then
+		git_clone https://github.com/wg/wrk.git ./tmp/wrk
+		cd ./tmp/wrk
+		make
+		sudo cp wrk /usr/local/bin
+	fi
+}
+
+install_aureola() {
+	git_clone https://github.com/erikdubois/Aureola ./tmp/Aureola/ 
+	./tmp/Aureola/install-conky.sh -y
+	curl -s -o $HOME/.config/conky/conky.conf https://raw.githubusercontent.com/fabiogibson/dev-machine/master/dotfiles/conky.conf 2>&1	
+}
+
+##################################################
+# Fonts
+##################################################
+install_powerline_fonts() {
+	if [ ! -f "$HOME/.local/share/fonts/Meslo LG L DZ Regular for Powerline.ttf" ]; then
+		git_clone https://github.com/powerline/fonts.git ./tmp/powerline_fonts/
+		./tmp/powerline_fonts/install.sh "Meslo LG L DZ Regular for Powerline"
+	fi
+}
+
+install_firacode() {
+	if [ ! -f "$HOME/.local/share/fonts/FiraCode-Regular.ttf" ]; then
+		git_clone https://github.com/tonsky/FiraCode.git ./tmp/firacode/
+		cp ./tmp/firacode/distr/ttf/* $HOME/.local/share/fonts/
+	fi
+}
+
+##################################################
+# Configuration Scripts
+##################################################
+create_virtual_env() {
+	pyenv install -s $1
+
+	if [ ! -d "$PYENV_ROOT/versions/$2" ]; then
+		echo Creating virtualenv $2 with Python $1
+		pyenv virtualenv $1 $2
+	else
+		echo Skipping virtualenv $2
+	fi
+}
+
 configure_git() {
 	echo Setting up git globals...
 	# use this hack to be able to invoke git diff instead of git difftool
 	echo '#!/bin/sh\nmeld $2 $5' | sudo tee /usr/local/bin/meld_git > /dev/null
+	/bin/cat <<EOM >/usr/local/bin/meld_git
+	#!/bin/sh
+	meld $2 $5
+	EOM
+
 	sudo chmod +x /usr/local/bin/meld_git
 
 	git config --global user.name "Fabio Gibson"
@@ -135,8 +153,25 @@ configure_git() {
 	git config --global diff.external meld_git	
 }
 
+##################################################
+# Machine setup goes here
+##################################################
+mkdir -p ~/tmp/
+pacman_install 	git
+
+# Setup python enviroments
+install_pyenv
+create_virtual_env 3.6.1 py3
+create_virtual_env 2.7.13 py2
+
+# install global python tools
+pip_install py3 powerline-status isort pep8 httpie
+pip_install py2 thefuck tox
+
+# set global python environments
+pyenv global 3.6.1 2.7.13 py3 py2
+
 pacman_install 				\
-	git				\
 	zsh				\
 	yaourt				\
 	yaourt-gui-manjaro		\
@@ -158,35 +193,25 @@ yaourt_install 			\
 	skypeforlinux-bin 	\
 	moka-icon-theme-git
 
-install_pyenv
-create_virtual_env 3.6.1 py3
-create_virtual_env 2.7.13 py2
-
-# install global python tools
-pip_install py3 youtube-dl powerline-status isort pep8 httpie
-pip_install py2 rename fabric thefuck tox
-
-# set global python environments
-pyenv global 3.6.1 2.7.13 py3 py2
-
+# install fonts
 install_powerline_fonts
+install_firacode
 
+# install tools
 install_wrk
-install_ranger
+install_aureola
+install_ohmyzsh
+npm_install mockserver browser-sync coffee-script typescript
 
-npm_install tern mockserver browser-sync coffee-script typescript
-
+# configure machine
+install_dotfiles
 configure_git
 
 # create workspace directories
 mkdir -p $HOME/.virtualenvs $HOME/Projects
-
-install_ohmyzsh
-install_dotfiles
 
 # switch shell to zsh
 chsh -s $(grep /zsh$ /etc/shells | tail -1)
 env zsh
 
 echo It\'s all done!
-exit 1
